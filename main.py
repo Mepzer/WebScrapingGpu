14ea6205728cc8db0f389c51929f6e0c0dbe4950
Mepzer <wesleyyads@gmail.com>
2022-03-14 16:19:20 -0300

    Disparando Mensagens via WhatsApp

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+import pywhatkit as pw
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# - Coletando dados
+URL = "https://www.kabum.com.br/produto/181088/processador-amd-ryzen-5-5600g-3-9ghz-4-4ghz-max-turbo-am4-video-integrado-6-nucleos-100-100000252box"
+
+headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39"}
+
+web = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(web.content, 'html.parser')
+
+price = soup.find('h4', itemprop='price').getText()
+title = soup.find('h1', itemprop='name').getText()
+
+# - Formatando o preço
+num_price = price[3:8]
+num_price = num_price.replace('.','')
+num_price = float(num_price)
+
+# - Disparando mensagem condicional
+if(num_price<1500):
+    msg = title + " Baixou de preço 😁!! Aproveita 💙!! " + price + "REAIS! Aproveita: https://www.kabum.com.br/produto/181088/processador-amd-ryzen-5-5600g-3-9ghz-4-4ghz-max-turbo-am4-video-integrado-6-nucleos-100-100000252box"
+    pw.sendwhatmsg("SEUNUMEROAQUI", msg, 16, 10)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
