ad536daa82dc785ce6cfb73755eab77f6f24efde
yuri-potatoq <47803046+yuri-potatoq@users.noreply.github.com>
2022-03-22 11:13:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 import pywhatkit as pw
 
 # - Coletando dados
-URL = "https://www.kabum.com.br/produto/181088/processador-amd-ryzen-5-5600g-3-9ghz-4-4ghz-max-turbo-am4-video-integrado-6-nucleos-100-100000252box"
+BASE_URL = "https://www.kabum.com.br"
+ENDPOINT = "/produto/181088"
+URL = BASE_URL + ENDPOINT
 
 headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39"
